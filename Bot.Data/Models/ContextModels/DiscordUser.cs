0b1128baa3b8a6b33de780c5b44392ca3b651f89
Jordan C <nykezgaming@live.com>
2022-05-01 23:02:05 -0400

    Create DiscordUser.cs

---
+++ Bot.Data/Models/ContextModels/DiscordUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Data.Models.ContextModels
+{
+    public class DiscordUser
+    {
+        [Key]
+        public string? DiscordId { get; set; }
+        public string? SteamId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
