42a7c52b18e887ac1f5dd41311fd14c9f354a103
Jordan C <nykezgaming@live.com>
2022-05-02 15:29:47 -0400

    Create IAuditEntity.cs

---
+++ Bot.Data/Models/IAuditEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Data.Models
+{
+    public interface IAuditEntity
+    {
+        string? CreatedByUser { get; set; }
+        DateTime CreatedAt { get; set; }
+        string UpdatedByUser { get; set; }
+        DateTime UpdatedAt { get; set; }
+    }
+}
