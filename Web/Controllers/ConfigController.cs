249e670849a8f7b5a34e8d921d3f8ba01f10b8f2
Jordan C <nykezgaming@live.com>
2022-05-09 13:01:40 -0400

    more web work

--- Web/Controllers/ConfigController.cs
+++ Web/Controllers/ConfigController.cs
@@ -1,8 +1,11 @@
 ﻿using Bot.Data.Models.ContextModels;
+using Bot.Data.Repos;
+using Bot.Data.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -10,15 +13,29 @@ namespace Web.Controllers
     public class ConfigController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly AppConfigService _appConfigService;
+        private readonly RolesRepo _rolesRepo;
 
-        public ConfigController(UserManager<AppUser> userManager)
+        public ConfigController(UserManager<AppUser> userManager, AppConfigService appConfigService, RolesRepo rolesRepo)
         {
             _userManager = userManager;
+            _appConfigService = appConfigService;
+            _rolesRepo = rolesRepo;
         }
 
         public async Task<IActionResult> Index()
         {
-            return View();
+            var viewModel = new ConfigViewModel
+            {
+                Roles = await _rolesRepo.GetAllRoles(),
+                Config = _appConfigService.GetConfig()
+            };
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View(new ScriptRole());
         }
     }
 }
---
+++ Web/Models/ConfigViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Bot.Data.Models;
+using Bot.Data.Models.ContextModels;
+
+namespace Web.Models
+{
+    public class ConfigViewModel
+    {
+        public AppConfiguration? Config { get; set; }
+        public List<ScriptRole>? Roles { get; set; }
+    }
+}
